785d85902664193538eba385be692cf1c0760bb0
Simone Risola #19 <71252421+Rixolino@users.noreply.github.com>
2024-10-18 08:27:12 +0000

    COMPLETO

--- backend/app.js
+++ backend/app.js
@@ -2,28 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-// Middleware per il parsing delle richieste in arrivo
+// Configurazione EJS
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Pagina web principale
 app.get('/', (req, res) => {
-    res.send(`
-        <form action="/greet" method="POST">
-            <label for="nome">Inserisci il tuo nome:</label>
-            <input type="text" id="nome" name="nome">
-            <button type="submit">Invia</button>
-        </form>
-    `);
+    res.render('form');
 });
 
-// Gestione della risposta POST
 app.post('/greet', (req, res) => {
-    const nome = req.body.nome;
-    res.send(`<h1>Ciao, ${nome}!</h1>`);
+    const name = req.body.name;
+    res.render('greet', { name: name });
 });
 
-// Avvio server
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Il server Server è in esecuzione su http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
